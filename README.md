7030861e59024c5e4469b7bedeeb12538bc6e401
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2022-05-01 15:21:28 -0600

    Fixed a file reference in test.sh

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ This project is intended to make testing the PPM easier.
 
 ## Steps
 ### Preparation
-1. Spin up PPM using standalone docker-compose script
-1. Exec into the container running the PPM
+1. Spin up PPM using standalone docker-compose script.
+1. Exec into the container running the PPM.
 
 ### While Testing
 1. Send test BSMs by invoking the test.sh script or the send_bsm.sh script.
-1. Observe the result by checking the info.log file in the logs directory in the container
+1. Observe the result by checking the info.log file in the logs directory in the container.
--- test.sh
+++ test.sh
@@ -2,14 +2,16 @@
 
 # Author: Daniel McCoy Stephenson
 
+# expected to be retained
 echo "Sending Test BSM: Normal, Inside Geofence - Expected Behavior: Retained"
 ./send_bsm.sh ./bsms/testbsm_normal_inside_geofence.txt
 
+echo "Sending Test BSM: PartII, Inside Geofence - Expected Behavior: Retained"
+./send_bsm.sh ./bsms/testbsm_partII_inside_geofence.txt
+
+# expected to be supressed
 echo "Sending Test BSM: Normal, Inside Geofence, Outside Speed Range (Above) - Expected Behavior: Supressed"
 ./send_bsm.sh ./bsms/testbsm_normal_inside_geofence_outside_speed_range_above.txt
 
 echo "Sending Test BSM: Normal, Inside Geofence, Outside Speed Range (Below) - Expected Behavior: Supressed"
 ./send_bsm.sh ./bsms/testbsm_normal_inside_geofence_outside_speed_range_below.txt
-
-echo "Sending Test BSM: PartII, Inside Geofence - Expected Behavior: Supressed"
-./send_bsm.sh ./bsms/testbsm_normal_inside_geofence_outside_speed_range_above.txt
