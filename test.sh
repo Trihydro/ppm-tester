c8ab94f939625d82178969c3288aca0d60c29089
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2022-05-01 15:31:58 -0600

    Added a BSM that is outside the geofence.

--- test.sh
+++ test.sh
@@ -10,6 +10,9 @@ echo "Sending Test BSM: PartII, Inside Geofence - Expected Behavior: Retained"
 ./send_bsm.sh ./bsms/testbsm_partII_inside_geofence.txt
 
 # expected to be supressed
+echo "Sending Test BSM: Normal, Outside Geofence - Expected Behavior: Supressed"
+./send_bsm.sh ./bsms/testbsm_normal_outside_geofence.txt
+
 echo "Sending Test BSM: Normal, Inside Geofence, Outside Speed Range (Above) - Expected Behavior: Supressed"
 ./send_bsm.sh ./bsms/testbsm_normal_inside_geofence_outside_speed_range_above.txt
 
